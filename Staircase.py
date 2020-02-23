53a4015e80b784d77fc816f95d2f977b3abfe22d
harshOS <harshithngr@gmail.com>
2020-02-23 12:05:31 +0530

    initial commit

--- Staircase.py
+++ Staircase.py
@@ -1,6 +1,6 @@
 def staircase(n):
     for i in range(1,n+1):
-        string = (n-i)*' ' + (i*'#')
+        string = (n-i)*' ' + (i*'#') # line to print for every loop
         print(string)
 
 staircase(6)
