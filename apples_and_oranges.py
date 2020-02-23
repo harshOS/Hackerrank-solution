3d185d653c132cc019965f1f6db676b55a6135bc
harshOS <harshithngr@gmail.com>
2020-02-23 12:02:21 +0530

    initial commit

---
+++ apples_and_oranges.py
@@ -0,0 +1,15 @@
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    a_dist = 0
+    o_dist = 0
+    a_count = 0
+    o_count = 0
+    for i in range(m):
+        a_dist = a + apples[i]
+        if a_dist >= s and a_dist <= t:
+            a_count += 1
+    print(a_count)
+    for i in range(n):
+        o_dist = b + oranges[i]
+        if o_dist >= s and o_dist <= t:
+            o_count += 1
+    print(o_count)
